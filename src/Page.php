889b7767b26a457a47a0c3383c724a8e0ac72d61
Maxime Rainville <maxime@silverstripe.com>
2023-07-11 22:01:53 +1200

    Add a multi link option

--- src/Page.php
+++ src/Page.php
@@ -2,6 +2,7 @@
 namespace MaximeRainville\SilverstripeLinkfieldTester;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\LinkField\Form\LinkField;
+use SilverStripe\LinkField\Form\MultiLinkField;
 use SilverStripe\LinkField\Models\Link;
 use SilverStripe\ORM\DataExtension;
 
@@ -11,8 +12,13 @@ class Page extends DataExtension
         'MyTestLink' => Link::class,
     ];
 
+    private static $has_many = [
+        'MyTestLinks' => Link::class,
+    ];
+
     public function updateCMSFields(FieldList $fields)
     {
         $fields->addFieldToTab('Root.LinkTest', LinkField::create('MyTestLink'));
+        $fields->addFieldToTab('Root.LinkTest', MultiLinkField::create('MyTestLinks'));
     }
 }
